d69928a15550ebeba5ac6065b81412d3384fd463
Ashle3 <ashleeb3@byui.edu>
2023-06-02 23:31:37 -0500

    adding more comments

--- src/App.java
+++ src/App.java
@@ -3,10 +3,8 @@ import java.util.Scanner; //import the Scanner class to read user input
 
 public class App {
     static int generateNumber(){
-        Random randomNum = new Random();
-
+        Random randomNum = new Random(); //create Random object
         int n = randomNum.nextInt(101); //generates a random number between 1 and 100
-
         return n;
     }
 
@@ -34,7 +32,9 @@ public class App {
             int userGuess = object.nextInt(); //reads the user's input
 
             if (userGuess == number){
+                System.out.println();
                 System.out.println("You got it! The answer is " + number);
+                System.out.println();
                 tries = -1;
             } else if (userGuess < number){
                 System.out.println();
@@ -48,14 +48,16 @@ public class App {
                 tries = removeTry(tries);
             }
         } 
-        
+
         object.close(); //closes the Scanner object
 
         if (tries == 0) {
             System.out.println("You've run out of turns. GAME OVER");
             System.out.println("The number was " + number);
+            //insert game summary
         } else if (tries == -1){
             System.out.println("Thank you for playing! ");
+            //insert game summary
         }
     }
 }
